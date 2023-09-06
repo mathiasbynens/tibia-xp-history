1e05a66fb543eb9ba307471c903cb1ab40ef2974
Mathias Bynens <mathias@qiwi.be>
2023-09-06 18:45:37 +0200

    Produce HTML output

--- analyze.mjs
+++ analyze.mjs
@@ -1,5 +1,7 @@
 import * as fs from 'node:fs/promises';
-import * as prettier from 'prettier';
+
+import {updateReadmeTable} from './render-markdown.mjs';
+import {updateHtml} from './render-html.mjs';
 
 const readJsonFile = async (fileName) => {
 	const json = await fs.readFile(fileName, 'utf8');
@@ -7,51 +9,27 @@ const readJsonFile = async (fileName) => {
 	return data;
 };
 
-const writeJsonFile = async (fileName, data) => {
-	const json = JSON.stringify(data, null, '\t') + '\n';
-	await fs.writeFile(fileName, json);
+const expForLevel = (level) => {
+	// https://tibia.fandom.com/wiki/Experience_Formula
+	return 50 / 3 * (level ** 3 - 6 * level ** 2 + 17 * level - 12);
 };
 
-const formatter = new Intl.NumberFormat('en');
-const format = (number) => {
-	return formatter.format(number);
-};
-const deltaFormatter = new Intl.NumberFormat('en', {
-	signDisplay: 'exceptZero',
-});
-const formatDelta = (number) => {
-	return deltaFormatter.format(number);
+const computeProgressWithinLevel = (level, experience) => {
+	const minExpForThisLevel = expForLevel(level);
+	const minExpForNextLevel = expForLevel(level + 1);
+	const delta = minExpForNextLevel - minExpForThisLevel;
+	const extra = experience - minExpForThisLevel;
+	const progress = Math.floor(extra / delta * 100);
+	return progress;
 };
 
-const showDelta = (value, hide = false, showZero = false) => {
-	if (!showZero && value === 0) return '';
-	if (hide) return '';
-	return `${formatDelta(value)}`;
-};
-
-const formatMarkdown = async (text) => {
-	const formatted = await prettier.format(text, {
-		parser: 'markdown'
-	});
-	return formatted.trimEnd();
-};
-
-const fixReadme = async (markdown) => {
-	const file = './README.md';
-	const readme = await fs.readFile(file, 'utf8');
-	const updated = readme.replace(
-		/(?<=<!-- START AUTO-UPDATED SECTION -->)([^<]+)(?=<!-- END AUTO-UPDATED SECTION -->)/,
-		`\n${markdown}\n`
-	);
-	await fs.writeFile(file, updated);
-};
-
-const createMarkdownTable = async (history) => {
-	const MARKDOWN_TABLE_LINES = [];
+const embellish = (history) => {
+	const embellished = [];
 	const first = { rank: -1, level: -1, experience: -1 };
 	const prev = { rank: -1, level: -1, experience: -1 };
 	const delta = { rank: -1, level: -1, experience: -1 };
 	let isFirst = true;
+	let lastDate;
 	for (const [date, entry] of history) {
 		if (isFirst) {
 			first.rank = entry.rank;
@@ -62,25 +40,47 @@ const createMarkdownTable = async (history) => {
 			delta.level = entry.level - prev.level;
 			delta.experience = entry.experience - prev.experience;
 		}
-		MARKDOWN_TABLE_LINES.push(`${date}|${entry.rank}|${showDelta(delta.rank, isFirst)}|${format(entry.experience)}|${showDelta(delta.experience, isFirst, true)}|${format(entry.level)}|${showDelta(delta.level, isFirst)}`);
+		embellished.push({
+			date: date,
+			rank: entry.rank,
+			rankDelta: isFirst ? null : delta.rank,
+			experience: entry.experience,
+			experienceDelta: isFirst ? null : delta.experience,
+			level: entry.level,
+			levelDelta: isFirst ? null : delta.level,
+			progressWithinLevel: computeProgressWithinLevel(entry.level, entry.experience),
+		});
 		isFirst = false;
 		prev.rank = entry.rank;
 		prev.level = entry.level;
 		prev.experience = entry.experience;
+		lastDate = date;
 	}
 	const last = prev;
 	delta.rank = last.rank - first.rank;
 	delta.level = last.level - first.level;
 	delta.experience = last.experience - first.experience;
-	MARKDOWN_TABLE_LINES.push(`**30 days**||**${showDelta(delta.rank)}**||**${showDelta(delta.experience, false, true)}**||**${showDelta(delta.level)}**`);
-	const markdown = await formatMarkdown(
-		'|date|rank|rank delta|experience|experience delta|level|level delta|\n|--|-:|-:|-:|-:|-:|-:|\n' +
-		MARKDOWN_TABLE_LINES.join('\n')
-	);
-	return markdown;
+	const meta = {
+		updated: lastDate,
+		days: history.size,
+		rankDelta: delta.rank,
+		experienceDelta: delta.experience,
+		levelDelta: delta.level,
+	};
+	const result = {
+		history: embellished,
+		meta: meta,
+	};
+	return result;
 };
 
+
+
 const HISTORY = new Map(Object.entries(await readJsonFile('./data/xp-history.json')));
-const HISTORY_LAST_30_DAYS = new Map([...HISTORY].slice(-30));
-const markdown = await createMarkdownTable(HISTORY_LAST_30_DAYS);
-await fixReadme(markdown);
+export const HISTORY_EMBELLISHED = embellish(HISTORY);
+
+// const HISTORY_LAST_30_DAYS = new Map([...HISTORY].slice(-30));
+
+await updateReadmeTable(HISTORY_EMBELLISHED);
+
+await updateHtml(HISTORY_EMBELLISHED);
